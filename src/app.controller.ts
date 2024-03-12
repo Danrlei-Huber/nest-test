38530985e6826c088e85d908243a9ffaea0f882d
Danrlei Huber <darleirafaelhuber@gmail.com>
2024-03-11 22:22:28 -0300

    alterando readme.md

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    const msg : string = 'ola mundo';
+    return msg;
+  }
+
+    @Get('say/:something')
+  getTest(@Param('something') word: string): string {
+    console.log('teste de console')
+    return word;
   }
 }
